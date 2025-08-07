5c1a72327d4c032d9add284138e1e901402fb4e3
shivang shukla <shivangshukla1271@gmail.com>
2025-08-07 16:06:12 +0530

    sum of two number is

--- calculator.java
+++ calculator.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 
 
 public class calculator {
+    public static void main (String args []){
     Scanner sc = new Scanner(System.in);
+    System.out.println("enter num1 :");
     int num1 = sc.nextInt();
+    System.out.println("enter a num2 :");
     int num2 = sc.nextInt();
     int sum = num1 + num2;
+    System.out.println("sum of number is:");
     System.out.println(sum);
+}
 }
