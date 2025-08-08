172d546a96279004783c454383507d850f2cba0b
shivang shukla <shivangshukla1271@gmail.com>
2025-08-08 12:46:17 +0530

    reverted star pattern

---
+++ patterns/revertedstar.java
@@ -0,0 +1,14 @@
+
+
+public class revertedstar {
+    public static void main(String[] args) {
+        for (int line =1; line<=5; line++) {
+            for (int star =1;star<=5-line +1;star++){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
