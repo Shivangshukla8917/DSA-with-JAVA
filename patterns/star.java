3a19b3ec3d743909c01d2d9e51300b0be886051f
shivang shukla <shivangshukla1271@gmail.com>
2025-08-08 12:45:58 +0530

    to print star like pattern

---
+++ patterns/star.java
@@ -0,0 +1,14 @@
+
+
+public class star {
+    public static void main(String[] args) {
+         int line =5;
+        for ( line =1; line <=5;line++){
+            for (int star =1; star <= line; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
